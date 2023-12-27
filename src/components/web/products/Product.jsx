371f5e11bfd0cc7ae524450816e70d623d0d406f
bayan7abdalhq <s12114374@stu.najah.edu>
2023-12-27 21:52:31 +0200

    change

--- src/components/web/products/Product.jsx
+++ src/components/web/products/Product.jsx
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import { useQuery } from 'react-query';
 import {  useParams } from 'react-router-dom';
-import ReactImageMagnify from 'react-image-magnify';
 import React, { useContext } from 'react';
 import { CartContext } from '../context/Cart.jsx';
 
@@ -29,24 +28,7 @@ function Product() {
     <div className="container">
       <div className="row">
         <div className="col-lg-4">
-          {data.subImages.map((img,index)=>
-          <React.Fragment key={index}>
-          <ReactImageMagnify {...{
-            smallImage: {
-                alt: 'Wristwatch by Ted Baker London',
-                isFluidWidth: true,
-                src: img.secure_url
-            },
-            largeImage: {
-                src: img.secure_url,
-                width: 1200,
-                height: 1800
-            },
-            isHintEnabled: true,
-        }} />
-        </React.Fragment>
-        )}
-        </div>
+         </div>
         <div className="col-lg-8">
         <h2>{data.name}</h2>
         <p>{data.price}</p>
